09fdca1599f74ffc60e7f1ad96bdfcc25fe88543
Piotr Zduniak <piotr@zduniak.net>
2015-02-01 13:28:17 +0100

    repeating the same action and expecting the result to change

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,4 @@
 $(document).ready(
 function(){ 
-$(".photos ul").innerfade({ speed: 'slow', timeout: 5000, type: 'random_start', containerheight: '250px'});
+$(".photos ul").innerFade({ speed: 'slow', timeout: 5000, type: 'random_start', containerheight: '250px'});
 });
