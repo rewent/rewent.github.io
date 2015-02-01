c02b8940c0eedabdfb845003561a6d16fc36a36f
Piotr Zduniak <piotr@zduniak.net>
2015-02-01 13:50:15 +0100

    wtf jquery

--- assets/projects.js
+++ assets/projects.js
@@ -3,21 +3,21 @@ function(){
 $(".projekty ul li span a").fancybox({'hideOnContentClick': true});
 $(".projekty span a").fancybox({'hideOnContentClick': true});
 
-$(".nazwaProjektu").toggle(function() {$(this).next("span").slideDown("normal");}, function() {$(this).next("span").slideUp("normal");});
+$(".nazwaProjektu").click(function() {$(this).next("span").slideDown("normal");}, function() {$(this).next("span").slideUp("normal");});
 
-$("#2008").toggle(function() {$(this).css("color","#c74600"); $(".2008").slideDown("normal");}, function() {$(this).css("color", "#10aa00"); $(".2008").slideUp("normal");});
-$("#2007").toggle(function() {$(this).css("color","#c74600"); $(".2007").slideDown("normal");}, function() {$(this).css("color","#10aa00"); $(".2007").slideUp("normal");});
-$("#2006").toggle(function() {$(this).css("color","#c74600"); $(".2006").slideDown("normal");}, function() {$(this).css("color","#10aa00"); $(".2006").slideUp("normal");});
-$("#nodate").toggle(function() {$(this).css("color","#c74600"); $(".nodate").slideDown("normal");}, function() {$(this).css("color","#10aa00"); $(".nodate").slideUp("normal");});
+$("#2008").click(function() {$(this).css("color","#c74600"); $(".2008").slideDown("normal");}, function() {$(this).css("color", "#10aa00"); $(".2008").slideUp("normal");});
+$("#2007").click(function() {$(this).css("color","#c74600"); $(".2007").slideDown("normal");}, function() {$(this).css("color","#10aa00"); $(".2007").slideUp("normal");});
+$("#2006").click(function() {$(this).css("color","#c74600"); $(".2006").slideDown("normal");}, function() {$(this).css("color","#10aa00"); $(".2006").slideUp("normal");});
+$("#nodate").click(function() {$(this).css("color","#c74600"); $(".nodate").slideDown("normal");}, function() {$(this).css("color","#10aa00"); $(".nodate").slideUp("normal");});
 
 $(".bm").hide();
-$("#bm").toggle(function() {$(this).css("color","#c74600"); $(".bm").fadeIn("fast");}, function() {$(this).css("color","#c10aa00"); $(".bm").fadeOut("fast");});
+$("#bm").click(function() {$(this).css("color","#c74600"); $(".bm").fadeIn("fast");}, function() {$(this).css("color","#c10aa00"); $(".bm").fadeOut("fast");});
 $(".bbu").hide();
-$("#bbu").toggle(function() {$(this).css("color","#c74600"); $(".bbu").fadeIn("fast");}, function() {$(this).css("color","#10aa00"); $(".bbu").fadeOut("fast");});
+$("#bbu").click(function() {$(this).css("color","#c74600"); $(".bbu").fadeIn("fast");}, function() {$(this).css("color","#10aa00"); $(".bbu").fadeOut("fast");});
 $(".ip").hide();
-$("#ip").toggle(function() {$(this).css("color","#c74600"); $(".ip").fadeIn("fast");}, function() {$(this).css("color","#c10aa00"); $(".ip").fadeOut("fast");});
+$("#ip").click(function() {$(this).css("color","#c74600"); $(".ip").fadeIn("fast");}, function() {$(this).css("color","#c10aa00"); $(".ip").fadeOut("fast");});
 $(".isc").hide();
-$("#isc").toggle(function() {$(this).css("color","#c74600"); $(".isc").fadeIn("fast");}, function() {$(this).css("color","#c10aa00"); $(".isc").fadeOut("fast");});
+$("#isc").click(function() {$(this).css("color","#c74600"); $(".isc").fadeIn("fast");}, function() {$(this).css("color","#c10aa00"); $(".isc").fadeOut("fast");});
 
 
 
