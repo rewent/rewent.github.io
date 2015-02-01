8afca725094239c732bf81ff7fba3694940a3781
Piotr Zduniak <piotr@zduniak.net>
2015-02-01 13:23:53 +0100

    kill me please

---
+++ assets/index.js
@@ -0,0 +1,4 @@
+$(document).ready(
+function(){ 
+$(".sztandarowe ul").innerfade({ speed: 'slow', timeout: 3500, type: 'random_start', containerheight: '105px'});
+});
---
+++ assets/script.js
@@ -0,0 +1,4 @@
+$(document).ready(
+function(){ 
+$(".photos ul").innerfade({ speed: 'slow', timeout: 5000, type: 'random_start', containerheight: '250px'});
+});
