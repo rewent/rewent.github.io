761d0450b2c36d70dac4780eb31d40f41012241b
Piotr Zduniak <piotr@zduniak.net>
2015-02-01 13:29:14 +0100

    ~Barack Obama 2016

--- assets/index.js
+++ assets/index.js
@@ -1,4 +1,4 @@
 $(document).ready(
 function(){ 
-$(".sztandarowe ul").innerfade({ speed: 'slow', timeout: 3500, type: 'random_start', containerheight: '105px'});
+$(".sztandarowe ul").innerFade({ speed: 'slow', timeout: 3500, type: 'random_start', containerheight: '105px'});
 });
